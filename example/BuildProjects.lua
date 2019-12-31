845a56f1005419b15ae5298e26f9381a3ed14879
Vaas_Kahn_Grim <29955667+VaasKahnGrim@users.noreply.github.com>
2019-12-31 11:05:53 -0600

    Importing stuff

--- example/BuildProjects.lua
+++ example/BuildProjects.lua
@@ -1,42 +1,58 @@
 vaas = {}
 include'ProjectConfig.lua'	--Includes the config
 
-local lua_state			--Auto handles project namming
+
+--You shouldn't mess with much below, most should be handled in the config file. Proceed below at own risk
+
+--Auto handles project namming and file name output
+local lua_state
 if vaas.serverside == false then
 	lua_state = 'gmcl_'
 elseif vaas.serverside == true then
 	lua_state = 'gmsv_'
 elseif vaas.serverside == 'shared' then
-	lua_state = 'gm_'	--You will still have to rename it later
+	lua_state = 'gm_'
+else
+	print"You didn't set vaas.serverside properly in the config file!!!"
+end
+if os.target() == 'windows' then
+	targetsuffix'_win32'
+elseif os.target() == 'macosx'then
+	targetsuffix'_osx'
+elseif os.target() == 'linux' then
+	targetsuffix'_linux'
+else
+	print'unknown target OS, wtf?'
 end
 
-solution(string.lower(lua_state..vaas.project_name))
-	language'C++' --What language is the project(this might end up moving to the config, but fuck C#.)
 
-	architecture'x86_64'	--Support that damn update facepunch is insistant on doing?
-
+workspace(string.lower(lua_state..vaas.project_name))
+	configurations{'Release','Debug'}
 	location(os.target()..'-'.._ACTION)
-	flags{'NoPCH'}
-
-	for k,v in pairs(vaas.solution_calls)do	--Applies Flags from the table for easy configuration
-		vaas.solution_calls.v()
-	end
-	
-	targetdir('lib/'..os.target()..'/')
-	includedirs{vaas.dependancies_folder}	--Handles dependancies folder from the config
-	
-	configurations
-	{ 
-		'Release'
-	}
-	
-	configuration'Release'
-		defines{'NDEBUG'}
-		for k,v in pairs(vaas.configuration_calls)do	--Flag handling
-			vaas.configuration_calls.v()
-		end
 
 	project(string.lower(lua_state..vaas.project_name))	--Name automatically converted to lower case to prevent stupid issues
-		defines{'GMMODULE'}
-		files{vaas.source_folder..'**.*',vaas.dependancies_folder..'**.*'}--Handles the source folder from the config
 		kind'SharedLib'
+		architecture'x86_64'	--Support that damn update facepunch is insistant on doing?	
+		language'C++' --What language is the project(this might end up moving to the config, but fuck C#.)
+		includedirs{vaas.dependancies_folder}	--Handles dependancies folder from the config
+		targetdir('lib/'..os.target()..'/')
+
+		defines{'GMMODULE'}
+
+		flags{'NoPCH'}
+		for k,v in pairs(vaas.solution_calls)do	--Applies Flags from the table for easy configuration
+			vaas.solution_calls.v()
+		end
+
+		configuration'Debug'
+			for k,v in pairs(vaas.debug_flags)do	--Flag handling
+				vaas.debug_flags.v()
+			end
+		configuration'Release'
+			defines{'NDEBUG'}
+			for k,v in pairs(vaas.release_flags)do	--Flag handling
+				vaas.release_flags.v()
+			end
+		configuration{}
+
+		files{vaas.source_folder..'**.*',vaas.dependancies_folder..'**.*'}--Handles the source folder from the config
