9cbeb7e617eff9582b892880a7cfc7a4bcbdf3e6
Vaas_Kahn_Grim <29955667+VaasKahnGrim@users.noreply.github.com>
2019-12-31 07:28:48 -0600

    ProjectConfig added

---
+++ example/ProjectConfig.lua
@@ -0,0 +1,14 @@
+vaas.project_name = 'vaas_example'
+vaas.serverside = true
+vaas.solution_calls = {
+	symbols'On',
+	editandcontinue'Off',
+	staticruntime'On',
+	vectorextensions'SSE'
+}
+vaas.configuration_calls = {
+	optimize'On',
+	floatingpoint'Fast'
+}
+vaas.dependancies_folder = '../include/'
+vaas.source_folder = 'src/'
